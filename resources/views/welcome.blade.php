54f28b18b43acdd93831b5d511ab5a3f96c2fdb6
Hikmalniuwb <hikmalpaijo2017@gmail.com>
2023-01-25 21:30:20 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+    
+</head>
+<body>
+    <div class="container mb-3">
+        <nav class="navbar navbar-expand-lg navbar-primary bg-secondary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">komentar</a>
+            </div>
+        </nav>
+    </div>
+    <livewire:berita />
+
+    @livewireScripts
+</body>
+</html>
